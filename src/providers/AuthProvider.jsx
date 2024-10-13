c8934c174b641269318bd3b29bab6dff865d8731
IztihadRafid <iztihadrafid10@gmail.com>
2024-10-13 13:24:47 +0600

    updated all

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from "../firebase/firebase.config";
+import axios from "axios";
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
@@ -26,10 +27,26 @@ const AuthProvider = ({ children }) => {
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            const userEmail = currentUser?.email || user?.email
+            const loggedUser= {email: userEmail};
             setUser(currentUser);
-            console.log('current user', currentUser);
+            //console.log('current user', currentUser);
             setLoading(false);
+            //if user exists then issue a token
+            if(currentUser){
+                axios.post('https://car-doctor-server-sand-two.vercel.app/jwt',loggedUser,{ withCredentials: true})
+                .then(res=>{
+                    //console.log('token response',res.data);
+                })
+            }
+            else{
+                axios.post('https://car-doctor-server-sand-two.vercel.app/logout',loggedUser,{withCredentials:true})
+                .then(res=>{
+                    console.log(res.data);
+                })
+            }
         });
+        
         return () => {
             return unsubscribe();
         }
